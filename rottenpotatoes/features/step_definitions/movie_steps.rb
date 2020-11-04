42ac596d5a383a68753a6c437ece75f4ad407768
Amritansh Saraf <amritansh@berkeley.edu>
2020-11-04 10:22:37 +0000

    fixed everything except one

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -13,25 +13,25 @@ Then /(.*) seed movies should exist/ do | n_seeds |
   Movie.count.should be n_seeds.to_i
 end
 
-When /^(?:|I )press "([^"]*)"$/ do |button|
-  click_button(button)
-end
+# When /^(?:|I )press "([^"]*)"$/ do |button|
+#   click_button(button)
+# end
 
-Then /^(?:|I )should see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_content(text)
-  else
-    assert page.has_content?(text)
-  end
-end
+# Then /^(?:|I )should see "([^"]*)"$/ do |text|
+#   if page.respond_to? :should
+#     page.should have_content(text)
+#   else
+#     assert page.has_content?(text)
+#   end
+# end
 
-Then /^(?:|I )should not see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_no_content(text)
-  else
-    assert page.has_no_content?(text)
-  end
-end
+# Then /^(?:|I )should not see "([^"]*)"$/ do |text|
+#   if page.respond_to? :should
+#     page.should have_no_content(text)
+#   else
+#     assert page.has_no_content?(text)
+#   end
+# end
 
 # When /^(?:|I )check "([^"]*)"$/ do |field|
 #   check(field)
